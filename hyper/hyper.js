7601f48dc6351b8651a460d9896e1c7ef3807c5e
j0can <josef.can@outlook.com>
2025-02-04 20:36:26 +0100

    hyper plugins

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -112,7 +112,7 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: [],
+    plugins: ["hyper-pane", "hyperline", "hyperterm-one-dark"],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed

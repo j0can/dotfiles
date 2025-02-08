40942fe0cd25419d604fcdf8617cf7023f14e358
j0can <josef.can@outlook.com>
2025-02-08 21:41:30 +0100

    keymaps for copy pasting becuse i could not lol

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,3 +7,7 @@ require('treesitter')
 require('completion')
 require('git')
 require('explorer')
+
+vim.api.nvim_set_keymap('i', '<C-v>', '<Esc>"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-c>', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('x', '<C-c>', '"+y', { noremap = true, silent = true })
